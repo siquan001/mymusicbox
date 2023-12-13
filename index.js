bdc196633cceea52d6f1b0abc33c0e6ecdb395da
siquan001 <chensiquan2022@163.com>
2023-12-13 19:47:48 +0800

    将所有字体图标换成内联SVG

--- index.js
+++ index.js
@@ -685,10 +685,12 @@ audio.oncanplay=function(){
 }
 audio.onplay=function(){
   playbtn.className='bi bi-pause playbtn';
+  playbtn.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause" viewBox="0 0 16 16">  <path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5m4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5"/></svg>'
   document.querySelector(".player").classList.add('play')
 }
 audio.onpause=function(){
   playbtn.className='bi bi-play-fill playbtn';
+  playbtn.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">  <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/></svg>'
   document.querySelector(".player").classList.remove('play')
 }
 audio.ontimeupdate=function(){
